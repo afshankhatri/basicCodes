ea21cc68fd375f2a53d378279aec54f86e7ca712
afshankhatri <mohdafshankhatri@gmail.com>
2025-06-21 23:05:12 +0530

    commit

---
+++ longestCommonPrefix.js
@@ -0,0 +1,23 @@
+
+
+
+// INCOMPLETE
+
+
+
+
+const strs = ["flower","flow","flight"]
+store = []
+
+for (let i = 0; i < strs.length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+        console.log(strs[i][j]);
+    }
+      
+}
+
+// try 2
+{
+    const strs = ["flower","flow","flight"]
+    const store = []
+}

2a8637af4bb32b3e3a869b555f152c6ce96415b6
afshankhatri <mohdafshankhatri@gmail.com>
2025-06-23 10:31:55 +0530

    commit

---
+++ removeSameElements.js
@@ -0,0 +1,13 @@
+// wap to remove same elements from the array . (elements ==numbers)
+
+const arr = [1,2,3,6,3,3,2]
+
+const store = []
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] ) {
+            
+        }
+    }
+    
+}console.log(store);

f418435fb653b83f219684f086744f172e178820
afshankhatri <mohdafshankhatri@gmail.com>
2025-06-26 08:13:20 +0530

    first commit

---
+++ projects/Quiz.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <windows.h>
+// #include <time.h>
+
+int Q1();
+int Q2();
+int Q3();
+int Q4();
+int Q5();
+
+void main()
+{
+    system("color 3F");
+    printf("\n\t\t\t~~~===Quizzzz Time===~~~\n\n");
+     Q1();
+     Q2();
+     Q3();
+     Q4();
+     Q5();
+
+}
+
+int Q1()
+{
+    printf("Q.> Who is the founder of C programming language?\n");
+    printf("\t1> Dennis Ritchie\n");
+    printf("\t2> Flying Brothers\n");
+    printf("\t3> Afshan Khatri\n");
+
+
+
+    int num;
+    scanf("%d",&num);
+    switch (num)
+    {
+    case 1:
+        printf("corrent answrer ");
+        break;
+    
+    case 2:
+        printf("wrong answer");
+        break;
+
+    case 3:
+        printf("wrong answer");
+        break;
+    default:
+        break;
+    }
+}
+int Q2()
+{
+    printf("\nQ.>afshan is the a lier?\n");
+    printf("\t1> only when he is in trouble\n");
+    printf("\t2> he always lies\n");
+    printf("\t3> no, he never lies\n");
+
+
+
+    int num;
+    scanf("%d",&num);
+    switch (num)
+    {
+    case 1:
+        printf("corrent answrer ");
+        break;
+    
+    case 2:
+        printf("wrong answer");
+        break;
+
+    case 3:
+        printf("wrong answer");
+        break;
+    default:
+        break;
+    }
+}
+int Q3()
+{
+    printf("\nQ.> Who is the founder of Al-froz chicken?\n");
+    printf("\t1> Afshan Khatri\n");
+    printf("\t2> shaikh hamdan\n");
+    printf("\t3> badshah khan\n");
+
+
+
+    int num;
+    scanf("%d",&num);
+    switch (num)
+    {
+    case 1:
+        printf("corrent answrer ");
+        break;
+    
+    case 2:
+        printf("wrong answer");
+        break;
+
+    case 3:
+        printf("wrong answer");
+        break;
+    default:
+        break;
+    }
+}
+int Q4()
+{
+    printf("\nQ.> Who is the founder of essence kitchen?\n");
+    printf("\t1> owais and his friend\n");
+    printf("\t2> owais and his brother\n");
+    printf("\t3> Afshan Khatri\n");
+
+
+
+    int num;
+    scanf("%d",&num);
+    switch (num)
+    {
+    case 1:
+        printf("corrent answrer ");
+        break;
+    
+    case 2:
+        printf("wrong answer");
+        break;
+
+    case 3:
+        printf("wrong answer");
+        break;
+    default:
+        break;
+    }
+}
+int Q5()
+{
+    printf("\nQ.> Who is the founder of KFC>?\n");
+    printf("\t1> dont know\n");
+    printf("\t2> Flying Brothers\n");
+    printf("\t3> Afshan Khatri\n");
+
+
+
+    int num;
+    scanf("%d",&num);
+    switch (num)
+    {
+    case 1:
+        printf("corrent answrer ");
+        break;
+    
+    case 2:
+        printf("wrong answer");
+        break;
+
+    case 3:
+        printf("wrong answer");
+        break;
+    default:
+        break;
+    }
+}
